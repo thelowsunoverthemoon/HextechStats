b83bf68238f0d97f781f66f08884c6c1c304c8b5
Lowsun <48923877+thelowsunoverthemoon@users.noreply.github.com>
2023-05-18 15:09:04 -0700

    Update main.go tabs

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
     "HTStats/profile"
     . "HTStats/endpoint"
     . "HTStats/base"
@@ -16,6 +16,7 @@ func main() {
     // router settings
     r := gin.Default()
 
+    // to allow API requests by change COR settings
     r.Use(CORSMiddleware())
     
     v1 := r.Group("/api/v1")
